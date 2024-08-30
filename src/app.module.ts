be64226912b79bbf6368a33495ae97c5dbf539fc
Cauan Diniz <cauanrodiniz@gmail.com>
2024-08-30 15:03:33 -0300

    chore: app module and dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { CryptoModule } from './infra/crypto/Crypto.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { JwtModule } from './infra/jwt/Jwt.module';
 import { UserModule } from './modules/user/user.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [CryptoModule, AuthModule, JwtModule, UserModule],
+  imports: [
+    CryptoModule,
+    AuthModule,
+    JwtModule,
+    UserModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'storage'),
+      serveStaticOptions: {
+        index: false,
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
